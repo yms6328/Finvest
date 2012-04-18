db752b2584e46423539ea9106768f8e35ce7f938
Hyojin Kim <kimhj6173@gmail.com>
2012-04-18 20:34:34 +0900

    index implementation

--- Finvest_API/Finvest_API/IndexFomular.h
+++ Finvest_API/Finvest_API/IndexFomular.h
@@ -1,8 +1,14 @@
 #ifndef INDEXFOMULAR_H
 #define INDEXFOMULAR_H
+#include <string>
+#include "SQLManager.h"
+#include "DBHelper.h"
 
 class IndexFomular
-{
+{public:
+        DBHelper db_helper; 
+
+        int ratio();
 
 };
 
--- Finvest_API/Finvest_API/StockIndex.cpp
+++ Finvest_API/Finvest_API/StockIndex.cpp
@@ -6,3 +6,7 @@
 */
 
 #include "StockIndex.h"
+
+//1. RSI
+
+//5. ratio of change(binary wave)
